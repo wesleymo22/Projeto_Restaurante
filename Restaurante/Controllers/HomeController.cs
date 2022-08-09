c8996d98235c8d7900e9e6484b80a76d8f0c6517
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-08-08 21:59:26 -0300

    Initial commit

---
+++ Restaurante/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurante.Models;
+using System.Diagnostics;
+
+namespace Restaurante.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
