a5098ed90a882a174e80871e21fa29096ad97658
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-08-24 16:39:54 -0300

    Implementando Carrinho de compras

--- Restaurante/Context/AppDbContext.cs
+++ Restaurante/Context/AppDbContext.cs
@@ -12,6 +12,7 @@ namespace Restaurante.Context
 
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
 
     }
 
--- Restaurante/Migrations/20220824193215_CarrinhoCompraItem.Designer.cs
+++ Restaurante/Migrations/20220824193215_CarrinhoCompraItem.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Restaurante.Context;
 
@@ -10,9 +11,10 @@ using Restaurante.Context;
 namespace Restaurante.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220824193215_CarrinhoCompraItem")]
+    partial class CarrinhoCompraItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
