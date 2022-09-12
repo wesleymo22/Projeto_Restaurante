c79cff9acf04da8ec03d97864b2103e7a5ba87ba
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-09-12 14:52:08 -0300

    configurando session e HttpContext

--- Restaurante/Startup.cs
+++ Restaurante/Startup.cs
@@ -20,7 +20,12 @@ public class Startup
         options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
         services.AddControllersWithViews();
+
+        services.AddMemoryCache();
+        services.AddSession();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +45,8 @@ public class Startup
         app.UseStaticFiles();
 
         app.UseRouting();
+
+        app.UseSession();
 
         app.UseAuthorization();
 
