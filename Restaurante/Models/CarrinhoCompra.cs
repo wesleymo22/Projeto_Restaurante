86d09570f150ab863e245fbd30753d5c6435843c
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-09-13 11:29:45 -0300

    Update Carrinho de compras

--- Restaurante/Models/CarrinhoCompra.cs
+++ Restaurante/Models/CarrinhoCompra.cs
@@ -1,6 +1,57 @@
-﻿namespace Restaurante.Models
+﻿using Restaurante.Context;
+
+namespace Restaurante.Models
 {
-    public class CarrinhCompra
+    public class CarrinhoCompra
     {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            //define uma sessão
+            ISession session =
+                services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            //obtem um serviço do tipo do nosso contexto
+            var context = services.GetService<AppDbContext>();
+
+            //gerar Id do carrinho
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            //atribui o id do carrinho na sessão
+            session.SetString("CarrinhoId", carrinhoId);
+
+            //retorna o carrinho com o contexto e o Id atribuido ou obtido
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+        }
+
+        public void AdicionaraoCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem =
+                _context.CarrinhoCompraItens.SingleOrDefault(
+                    s => s.Lanche.LancheId == lanche.LancheId &&
+                    s.CarrinhoCompraId == CarrinhoCompraId);
+            if (carrinhoCompraItem == null)
+            {
+                carrinhoCompraItem = new CarrinhoCompraItem
+                {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Lanche = lanche,
+                    Quantidade = 1
+                };
+                _context.CarrinhoCompraItens.Add(carrinhoCompraItem);
+            }
+        }
     }
 }
