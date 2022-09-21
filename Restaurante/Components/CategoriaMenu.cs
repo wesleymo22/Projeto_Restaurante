aa1b63310a53ef6e64c64ae2763c7aa6efd97fd5
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-09-21 09:21:31 -0300

    View Component exibindo as categorias menu Lanche

---
+++ Restaurante/Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurante.Repositories.Interfaces;
+
+namespace Restaurante.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categoria = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categoria);
+        }
+    }
+}
