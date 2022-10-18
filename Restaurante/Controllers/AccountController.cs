8c06745b3aa6e95769bdf666b4ebede44fcf5878
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-10-18 15:02:02 -0300

    Implementando o login

---
+++ Restaurante/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Restaurante.ViewModels;
+
+namespace Restaurante.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManeger;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManeger,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManeger = userManeger;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (ModelState.IsValid)
+               return View(loginVM);
+
+            var user = await _userManeger.FindByNameAsync(loginVM.UserName);
+
+            if(user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(loginVM.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(loginVM.ReturnUrl);
+                }
+            }
+            ModelState.AddModelError("", "Falha ao realizar o login!");
+                return View(loginVM);
+        }
+    }
+}
---
+++ Restaurante/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurante.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o Nome")]
+        [Display(Name = "Usuario")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Informe a Senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
