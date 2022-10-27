fed8d6d6e614e5d956a44523264597568ef1e3e5
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-10-27 13:45:35 -0300

    [Authorize(Roles = "Admin")]

--- Restaurante/Areas/Admin/Controllers/AdminCategoriasController.cs
+++ Restaurante/Areas/Admin/Controllers/AdminCategoriasController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using Restaurante.Models;
 namespace Restaurante.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminCategoriasController : Controller
     {
         private readonly AppDbContext _context;
