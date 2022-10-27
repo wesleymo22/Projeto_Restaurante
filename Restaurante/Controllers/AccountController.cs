e06514f6103ab954a59ba0f8d1225787fb720ba0
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-10-27 11:40:05 -0300

    Acesso negado View

--- Restaurante/Controllers/AccountController.cs
+++ Restaurante/Controllers/AccountController.cs
@@ -88,5 +88,10 @@ namespace Restaurante.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
+
+        public IActionResult AccessDenied()
+        {
+            return View(); 
+        }
     }
 }
