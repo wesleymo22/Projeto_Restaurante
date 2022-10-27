babf4bb6870242c96b2dce0799601f3aec519180
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-10-27 09:01:03 -0300

    Criaçao e Configuraçao do AdminController

--- Restaurante/Startup.cs
+++ Restaurante/Startup.cs
@@ -66,7 +66,7 @@ public class Startup
         {
             endpoints.MapControllerRoute(
               name: "areas",
-              pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+              pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
             );
 
             endpoints.MapControllerRoute(
